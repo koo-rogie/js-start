07cf35ce6717d678aba8c6f71dfe739ad497e2a2
koo-rogie <rn5184@naver.com>
2025-03-31 11:47:04 +0900

    📝 dosc: 백준 문제 풀이 완료

--- boj/02/04.js
+++ boj/02/04.js
@@ -46,4 +46,17 @@
 */
 
 const fs = require("fs");
-const fileData = fs.readFileSync(0).toString().trim().split(" ");
+const fileData = fs.readFileSync(0).toString().trim().split("\n");
+
+let X = parseInt(fileData[0]);
+let Y = parseInt(fileData[1]);
+
+if (X > 0 && Y > 0) {
+  console.log("1");
+} else if (X < 0 && Y > 0) {
+  console.log("2");
+} else if (X < 0 && Y < 0) {
+  console.log("3");
+} else {
+  console.log("4");
+}
