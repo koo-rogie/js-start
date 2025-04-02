5843ac4e82410b1465a51f787407d0edcedfb7fe
koo-rogie <rn5184@naver.com>
2025-04-02 16:28:10 +0900

    🚚 rename: 기존 21번 파일명을 21-01로 수정

---
+++ workspace/ch01/ex01-21-01.js
@@ -0,0 +1,43 @@
+/*
+반복문 - for문
+*/
+
+// 1부터 10까지의 모든 수의 합계를 출력
+let sum = 0;
+for (let i = 1; i <= 10; i++) {
+  // 초기화, 조건식, 증감식
+  sum += i;
+}
+console.log("합계: ", sum);
+
+// 1부터 n까지 더하면 10,000이 넘을까?
+sum = 0;
+let count = 1;
+while (sum < 10000) {
+  sum += count;
+  count++;
+}
+
+console.log(--count, sum);
+// ---------------------------------
+
+//  1~10까지 합계
+let i = 1;
+sum = 0;
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+console.log("합계(for):", sum);
+
+// ---------------------------------
+
+i = 2;
+
+for (let i = 1; i <= 10; i++) {
+  sum += i;
+}
+console.log("홀수합게: ", sum);
+for (let i = 1; i <= 10; i += 2) {
+  sum += i;
+}
+console.log("Wkr수합게: ", sum);
