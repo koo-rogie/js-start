272a9a29754f28dae8f53ab92ccb5996b3697896
koo-rogie <rn5184@naver.com>
2025-04-02 17:59:55 +0900

    📝 dosc: 주석제거

--- workspace/ch01/ex01-20-01.js
+++ workspace/ch01/ex01-20-01.js
@@ -6,9 +6,9 @@
 let i = 0;
 
 //0 부터 10까지 출력
-while (i <= 10) {
-  console.log("1부터 10까지 출력: ", i++);
-}
+// while (i <= 10) {
+//   console.log("1부터 10까지 출력: ", i++);
+// }
 
 // 1부터 10까지의 모든 수의 합계를 출력
 let sum = 0;
@@ -18,7 +18,6 @@ while (i <= 10) {
   sum += i; // 실행 할 코드
   i++; // 증감식
 }
-console.log(i);
 console.log(sum);
 // while (i <= 10) {
 //   a += i;
