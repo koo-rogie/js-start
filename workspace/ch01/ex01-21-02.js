ead7f2447c2bd38907ad706f209162074af4381a
koo-rogie <rn5184@naver.com>
2025-04-02 16:28:38 +0900

    📝 dosc: 수업 파일 등록

---
+++ workspace/ch01/ex01-21-02.js
@@ -0,0 +1,87 @@
+/*
+for문
+*/
+
+/*
+ *
+ **
+ ***
+ ****
+ *****
+ */
+
+const lines = 10;
+
+for (let i = 0; i < lines; i++) {
+  let star = "";
+  for (let k = 0; k <= i; k++) {
+    star += "🌟";
+  }
+  console.log(star);
+}
+
+/*
+ *****
+ ****
+ ***
+ **
+ *
+ */
+
+/*
+ *****
+| ****
+|| ***
+||| **
+|||| *
+ */
+
+for (let i = lines; i >= 1; i--) {
+  let star = "";
+  for (let k = 0; k < i; k++) {
+    star += "🌟";
+  }
+  console.log(star);
+}
+
+/*
+|||| *
+||| **
+|| ***
+| ****
+*****
+
+ */
+
+for (let i = 0; i < lines; i++) {
+  let space = "";
+  let star = "";
+  for (let k = 0; k < lines - i - 1; k++) {
+    space += "  ";
+  }
+  for (let k = 0; k <= i; k++) {
+    star += "🌲";
+  }
+  console.log(space + star);
+}
+
+for (let i = 0; i < lines; i++) {
+  let space = "";
+  let star = "";
+  for (let k = 0; k < lines - i - 1; k++) {
+    space += "  ";
+  }
+  for (let k = 0; k <= i; k++) {
+    star += "✨";
+  }
+  console.log(space + star);
+} 
+
+/*
+||| *
+|| ***
+| *****
+|| ***
+||| *
+
+ */
