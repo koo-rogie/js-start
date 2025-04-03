3ceb1047c1a5c77fed8595f5de7262633a32cf2e
koo-rogie <rn5184@naver.com>
2025-04-03 09:18:50 +0900

    ♻️ ref: 어제 수업 듣고 수정

--- workspace/ch01/ex01-20-03.js
+++ workspace/ch01/ex01-20-03.js
@@ -21,11 +21,11 @@ let sum = 0;
 while (sum < 1000_0000) {
   // 날짜가 n이 되는 날까지
   sum += count;
-  day++;
   count *= 2;
+  day++;
 }
 console.log(
-  `${--day}일 동안 받았더니 ${sum}개 됐다. 내일은 ${count}개 받을 수 있겠군.`
+  `${day}일 동안 받았더니 ${sum}개 됐다. 내일은 ${count}개 받을 수 있겠군.`
 );
 
 // 30일 동안 몇개의 쌀알을 모을 수 있는가
@@ -34,11 +34,12 @@ day = 1;
 count = 1;
 sum = 0;
 
-while (day <= 30) { // 30일 까지 반복
+while (day <= 30) {
+  // 30일 까지 반복
   sum += count; // 받은 쌀알을 합산함
-  day++; // 날짜를 하루씩 더함
   count = count * 2; // 쌀알을 2배씩 늘려줌
+  day++; // 날짜를 하루씩 더함
 }
 console.log(
-  `${--day}일 동안 받았더니 ${sum}개 됐다. 내일은 ${count}개 받을 수 있겠군.`
+  `${day}일 동안 받았더니 ${sum}개 됐다. 내일은 ${count}개 받을 수 있겠군.`
 );
