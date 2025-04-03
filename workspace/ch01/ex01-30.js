2d5edc8631448e614bad433739f44daee91ac105
koo-rogie <rn5184@naver.com>
2025-04-03 17:16:44 +0900

    📝 docs: funciton 첫 만남

--- workspace/ch01/ex01-30.js
+++ workspace/ch01/ex01-30.js
@@ -2,5 +2,16 @@
 기본 함수
 */
 
+console.log("hello Function!");
+console.log("hello Function!");
+console.log("hello Function!");
 
-
+//함수 생성
+function sayHello() {
+  console.log("안녕 Function!");
+}
+//함수 호출
+sayHello();
+for (let i = 0; i < 12; i++){
+  sayHello();
+}
--- workspace/ch01/ex01-33.js
+++ workspace/ch01/ex01-33.js
@@ -2,4 +2,35 @@
 반환값이 있는 함수
 */
 
+// 10+100의 결과를 출력 하는 함수 (add)
+function add() {
+  let n1 = 10;
+  let n2 = 100;
+  let result = n1 + n2;
+  console.log(result);
+}
 
+// 전달받은 숫자와 100의 합계를 출력하는 함수 (add100)
+function add100(num) {
+  let ten = 100;
+  console.log(num + ten);
+}
+// 전달받은 두 숫자의 합계를 출력하는 함수(sum)
+function getsum(num1, num2) {
+  let result = num1 + num2;
+  // console.log(result);
+  return result; // 위의 값이 반환됐다.
+}
+
+
+add();
+add();
+
+add100(10);
+add100(20);
+
+// getsum(10, 20);
+// getsum(30, 40);
+
+let returnVal = getsum(52, 100);
+console.log(returnVal);
