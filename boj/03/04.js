03acfcebafea3d575bb53a1e79ebae00ecfbbee8
koo-rogie <rn5184@naver.com>
2025-04-07 19:52:23 +0900

    📝 docs: 백준 문제 풀었음, 다만 내가 푼게 아니라서 다시 풀어야함

--- boj/03/04.js
+++ boj/03/04.js
@@ -139,18 +139,21 @@ No
 const fs = require("fs");
 const fileData = fs.readFileSync(0).toString().trim().split("\n");
 
-const X = parseInt(fileData[0]); // 총 가격을 더한것
-let T = parseInt(fileData[0]); //라인수
+let totalPrice = parseInt(fileData[0]); // 총합
+let T = parseInt(fileData[1]); // 라인개수
+// let dataPlus;
+let total = 0;
 
 for (let i = 2; i < T + 2; i++) {
   const data = fileData[i].split(" "); // i번째 줄에서 스페이스로 나누고
-  const A = parseInt(data[0]); // i줄에서 첫번째로 받는 케이스가 A
-  const B = parseInt(data[1]); // i 줄에서 두번째로 받는 케이스가 B
-  console.log(A + B); // A와 B를 더한다
+  const A = parseInt(data[0]); // i줄에서 첫번째로 받는 값이 A
+  const B = parseInt(data[1]); // i줄에서 두번째로 받는 값이 B
+
+  total += A * B; // A와 B를 곱한 값을 total에 더한다
 }
 
-
-// let allMplus = parseInt(fileData[0]);
-// let allLplus = parseInt(fileData[1]);
-// let listMoney = parseInt(fileData[2]);
-// let listcount = parseInt(fileData[3]);
+if (totalPrice === total) {
+  console.log("Yes"); // 총합과 같으면 Yes
+} else {
+  console.log("No"); // 총합과 같으면 Yes
+}
