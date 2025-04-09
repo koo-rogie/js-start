c8e47579cce4fe74400c056e9ba98ecc26c4cd7d
koo-rogie <rn5184@naver.com>
2025-04-09 17:22:12 +0900

    💬Comment : 프로그램에서 바로 쓰기 위한 주석 제거

---
+++ boj/input/sample.js
@@ -0,0 +1,31 @@
+function main() {
+  const data = getData();
+
+  console.log(data);
+}
+main();
+
+/**
+ * 표준 입력장치(콘솔)에서 두 줄로 입력된 줄당 두 건의 데이터를 읽어서 숫자로 변환한 후
+ * 배열로 저장하여 반환한다.
+ * @returns {object} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  const fileData = fs.readFileSync(0).toString();
+  const arr = fileData.trim().split("\n");
+
+  const result = [];
+  for (let i = 0; i < arr.length; i++) {
+    const row = arr[i];
+    const rowArr = row.split(" ");
+
+    for (let k = 0; k < rowArr.length; k++) {
+      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
+    }
+
+    result.push(rowArr);
+  }
+
+  return result;
+}
