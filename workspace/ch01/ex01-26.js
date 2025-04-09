4921e3f6cc307e8e3aa5f55173ab502b2e289236
koo-rogie <rn5184@naver.com>
2025-04-09 13:23:01 +0900

    💬Comment : 주석 추가

--- workspace/ch01/ex01-26.js
+++ workspace/ch01/ex01-26.js
@@ -3,6 +3,7 @@
 */
 
 const arr = new Array(1, 2, 3); // 빈 배열 생성 및 초기화
+const arr2 = [1, 2, 3]; // json 배열 생성 및 초기화
 console.log("arr의 배열과 타입은?: ", typeof arr, arr); //  object [ 1, 2, 3 ]
 console.log("arr의 길이는?: ", arr.length); // 0
 
@@ -11,10 +12,11 @@ arr.push(10); // 배열에 값 추가
 console.log("arr의 바뀐 길이는?: ", arr.length); // 0
 console.log("arr의 바뀐 배열과 타입은?: ", typeof arr, arr); // object [ 1, 2, 3, 10 ]
 
-arr[4] = 20; // 배열에 값 추가
-arr[5] = 30; // 배열에 값 추가
-arr[1] = 100; // 배열에 값을 수정
-arr[9] = 200; // 배열에 값을 수정
+arr[1] = 100; // [ 1, 100, 3, 10 ]
+arr[4] = 20; // [ 1, 100, 3, 10, 20 ]
+arr[5] = 30; //  [ 1, 100, 3, 10, 20, 30 ]
+arr[9] = 200; // [ 1, 100, 3, 10, 20, 30, <3 empty items>, 200 ]
+
 console.log("arr의 index의 길이는?: ", arr.length); // 0
 console.log("arr의 index의 배열과 타입은?: ", typeof arr, arr); // object [ 1, 2, 3, 10, 20, 30 ]
 console.log("arr의 index 7번의 값은?: ", arr[7]); // undefined
