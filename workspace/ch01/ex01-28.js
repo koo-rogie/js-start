14b7ad12b7f3523bb32f7b2eb1ef874746222279
koo-rogie <rn5184@naver.com>
2025-04-09 17:23:29 +0900

    📝 docs: 배열 첫만남

--- workspace/ch01/ex01-28.js
+++ workspace/ch01/ex01-28.js
@@ -2,3 +2,44 @@
 유사 배열 객체
 */
 
+// 배열 생성
+const colorArr = ["orange", "yellow", "geen"];
+colorArr.push("blue"); // 배열에 요소 추가
+/**
+ * 매개 변수로 전달받은 배열의 모든 요소를 인덱스와 함께 출력한다
+ * @param {[]} arr - 배열
+ */
+
+function printArr(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    console.log(`배열의 ${i}번째인 ${arr[i]}을 출력합니다`);
+  }
+}
+/*
+유사 배열 객체
+ - length속성
+ - 0부서 시작해서 1씩 증가하는 속성 추가
+*/
+printArr(colorArr); // printArr 함수 호출
+
+const arr = {
+  length: 3,
+  0: "orange",
+  1: "yellow",
+  2: "green",
+  /**
+   * 배열의 맨 끝에 지정한 elem를 추가한다(이 캐이스는 유사 배열이니 주의)
+   * @param {string} elem - 추가할 요소
+   */
+  push: function (elem) {
+    arr[arr.length] = elem;
+    arr.length++;
+  },
+};
+arr.push("black");
+arr.push("white");
+arr.push("안녕");
+arr.push("하세용");
+printArr(arr);
+// 문자열도 유사 배열 객채의 일종
+// printArr("이것은 문자열");
