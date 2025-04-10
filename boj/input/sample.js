def56bee9f60d12a044d23ec0e71ec72db706a7a
koo-rogie <rn5184@naver.com>
2025-04-10 10:34:36 +0900

    ♻️ ref: t 선생님 코드에서 getDate 부분을 축약함

--- boj/input/sample.js
+++ boj/input/sample.js
@@ -1,31 +1,22 @@
-function main() {
-  const data = getData();
-
-  console.log(data);
-}
-main();
-
 /**
- * 표준 입력장치(콘솔)에서 두 줄로 입력된 줄당 두 건의 데이터를 읽어서 숫자로 변환한 후
+ * 표준 입력장치(콘솔)에서 여러줄로 입력된 줄당 여러 건의 데이터를 읽어서 숫자로 변환한 후
  * 배열로 저장하여 반환한다.
- * @returns {object} 2차원 배열
+ * @returns {[]} 2차원 배열
  */
 function getData() {
   const fs = require("fs");
-  const fileData = fs.readFileSync(0).toString();
-  const arr = fileData.trim().split("\n");
-
-  const result = [];
-  for (let i = 0; i < arr.length; i++) {
-    const row = arr[i];
-    const rowArr = row.split(" ");
-
-    for (let k = 0; k < rowArr.length; k++) {
-      rowArr[k] = isNaN(rowArr[k]) ? rowArr[k] : parseInt(rowArr[k]);
-    }
-
-    result.push(rowArr);
-  }
-
-  return result;
+  return fs
+    .readFileSync(0)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((row) =>
+      row.split(" ").map((val) => (isNaN(val) ? val : parseInt(val)))
+    );
 }
+
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+}
+main();
