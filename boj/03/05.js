85d66ee442a87f7cfee121191d3c9ac0e079359b
koo-rogie <rn5184@naver.com>
2025-04-10 11:14:24 +0900

    ♻️ref : 선생님이랑 같이 풀기, 내가 꼭 다시 풀어야함

--- boj/03/05.js
+++ boj/03/05.js
@@ -47,11 +47,39 @@ long int
 예제 출력 2
 long long long long long int
 */
-//TODO-오늘 은 이 문제를 풀어보자
-const fs = require("fs");
-const fileData = fs.readFileSync(0).toString().trim().split(" ");
+/**
+ * 표준 입력장치(콘솔)에서 여러줄로 입력된 줄당 여러 건의 데이터를 읽어서 숫자로 변환한 후
+ * 배열로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  return fs
+    .readFileSync(0)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((row) =>
+      row.split(" ").map((val) => (isNaN(val) ? val : parseInt(val)))
+    );
+}
 
-const N = parseInt(fileData[0]);
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  const bytes = data[0][0];
+  const longTiems = bytes / 4;
+  let result = "long ".repeat(longTiems);
+  /*
+  // 1안
+  let result = "";
+  for (let i = 0; i < longTiems; i++) {
+    result += "long ";
+  }
+  */
+  console.log(result + "int");
+}
+main();
 
 // 4바이트에서는 즉 4글자에서는 long int
 // 8바이트에서는 long long int
