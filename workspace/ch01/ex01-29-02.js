fe47b02aabc9e5eb806b9fe161bf860f2947563b
koo-rogie <rn5184@naver.com>
2025-04-10 17:03:56 +0900

    📝Docs : n차원 배열

---
+++ workspace/ch01/ex01-29-02.js
@@ -0,0 +1,30 @@
+/*
+2차원 배열
+fe 수강생의 이름을 저장하는 배열을 작성해보자.'
+*/
+
+// 1차원 배열
+const festudents13 = ["임한길", "이호정", "김현지", "이도율", "박선영", "김하영"];
+const festudents12 = ["이서윤", "김지안", "노윤호", "장하린", "백도윤", "오예린"];
+const festudents11 = ["한지우", "류서준", "배가은", "민태윤", "서하진", "조은채"];
+// 2차원 배열
+const festudents = [festudents13, festudents12, festudents11];
+
+// 1차원 배열
+const bestudents13 = ["정태훈", "윤도현", "박서진", "김하윤", "오세찬", "최다인"];
+const bestudents12 = ["이주호", "강민하", "배연우", "문가람", "홍준기", "신나영"];
+const bestudents11 = ["조하민", "류예진", "김태영", "장유나", "서민재", "노지후"];
+
+// 2차원 배열
+const bestudents = [bestudents13, bestudents12, bestudents11];
+
+// 1차원 배열
+const dsstudents13 = ["이정우", "안세연", "황도윤", "백하윤", "임채은", "강서준"];
+const dsstudents12 = ["최지안", "김성훈", "박다은", "송하린", "정시우", "조은지"];
+const dsstudents11 = ["윤지훈", "서예빈", "오민석", "장유림", "배시현", "하도연"];
+
+// 2차원 배열
+const dsstudents = [dsstudents13, dsstudents12, dsstudents11];
+// 3차원 배열
+const students = [festudents, bestudents, dsstudents];
+console.log(students);
