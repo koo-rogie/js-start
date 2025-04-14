e40736f085fdf195d89e457376c76d6265442bbe
koo-rogie <rn5184@naver.com>
2025-04-14 13:25:36 +0900

    🚧 work: 추가로 풀어볼 것

--- boj/04/01.js
+++ boj/04/01.js
@@ -39,3 +39,35 @@
 1
 */
 
+/**
+ * 표준 입력장치(콘솔)에서 여러줄로 입력된 줄당 여러 건의 데이터를 읽어서 숫자로 변환한 후
+ * 배열로 저장하여 반환한다.
+ * @returns {[]} 2차원 배열
+ */
+function getData() {
+  const fs = require("fs");
+  return fs
+    .readFileSync(0)
+    .toString()
+    .trim()
+    .split("\n")
+    .map((row) =>
+      row.split(" ").map((val) => (isNaN(val) ? val : parseInt(val)))
+    );
+}
+
+function main() {
+  const data = getData();
+  // data에서 값을 꺼내서 문제 해결하는 코드 작성
+  //   첫째 줄에 정수의 개수 N(1 ≤ N ≤ 100)이 주어진다.
+  const n = data[0];
+  // 둘째 줄에는 정수가 공백으로 구분되어져있다.
+  const v = data[1].split(" ");
+  // 셋째 줄에는 찾으려고 하는 정수 v가 주어진다.
+  let sumTrue = 0;
+
+  for (let i = 0; i <= n; i++) {
+
+  }
+}
+main();
