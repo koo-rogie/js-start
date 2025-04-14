fdb6741769e6644b32980c8167ba018fd3381b06
koo-rogie <rn5184@naver.com>
2025-04-14 16:43:01 +0900

    📝 docs: 05 클라이언트 사이드 예제 추가

---
+++ workspace/ch05/ex05-02-01.js
@@ -0,0 +1,26 @@
+// 처음버전
+let plusbtn = document.querySelector(".plus");
+let minusbtn = document.querySelector(".minus");
+let resetbtn = document.querySelector(".reset");
+let countSpan = document.querySelector("#container span");
+let count = 0;
+// const count = parseInt(countSpan.firstChild.nodeValue);
+
+plusbtn.addEventListener("click", () => {
+  // 플러스를 클릭했을때
+  // countSpan.firstChild.nodeValue = count + 1;
+  count++;
+  countSpan.textContent = count;
+});
+minusbtn.addEventListener("click", () => {
+  // 마이너스를 클릭했을때
+  count--;
+  countSpan.textContent = count;
+});
+resetbtn.addEventListener("click", () => {
+  // 리셋을 클릭했을때
+  count = 0;
+  countSpan.textContent = count;
+});
+
+// const count = parseInt(countSpan.firstChild.nodeValue);
