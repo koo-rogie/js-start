57577655426f3c0c79f8a2efe050544a54654442
koo-rogie <rn5184@naver.com>
2025-04-16 09:18:00 +0900

    📝 docs: 브라우저가 랜더링이 된 이후 실행하라는 코드 추가

--- workspace/ch05/ex05-02-01.js
+++ workspace/ch05/ex05-02-01.js
@@ -1,26 +1,28 @@
-// 처음버전
-let plusbtn = document.querySelector(".plus");
-let minusbtn = document.querySelector(".minus");
-let resetbtn = document.querySelector(".reset");
-let countSpan = document.querySelector("#container span");
-let count = 0;
-// const count = parseInt(countSpan.firstChild.nodeValue);
+document.addEventListener("DOMContentLoaded", () => {
+  // 처음버전
+  let plusbtn = document.querySelector(".plus");
+  let minusbtn = document.querySelector(".minus");
+  let resetbtn = document.querySelector(".reset");
+  let countSpan = document.querySelector("#container span");
+  let count = 0;
+  // const count = parseInt(countSpan.firstChild.nodeValue);
 
-plusbtn.addEventListener("click", () => {
-  // 플러스를 클릭했을때
-  // countSpan.firstChild.nodeValue = count + 1;
-  count++;
-  countSpan.textContent = count;
-});
-minusbtn.addEventListener("click", () => {
-  // 마이너스를 클릭했을때
-  count--;
-  countSpan.textContent = count;
-});
-resetbtn.addEventListener("click", () => {
-  // 리셋을 클릭했을때
-  count = 0;
-  countSpan.textContent = count;
-});
+  plusbtn.addEventListener("click", () => {
+    // 플러스를 클릭했을때
+    // countSpan.firstChild.nodeValue = count + 1;
+    count++;
+    countSpan.textContent = count;
+  });
+  minusbtn.addEventListener("click", () => {
+    // 마이너스를 클릭했을때
+    count--;
+    countSpan.textContent = count;
+  });
+  resetbtn.addEventListener("click", () => {
+    // 리셋을 클릭했을때
+    count = 0;
+    countSpan.textContent = count;
+  });
 
-// const count = parseInt(countSpan.firstChild.nodeValue);
+  // const count = parseInt(countSpan.firstChild.nodeValue);
+});
