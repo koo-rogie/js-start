707ec0114e3dc17619e4986862b7c48c9ef52d50
koo-rogie <rn5184@naver.com>
2025-04-22 20:24:13 +0900

    💬Comment : 필요없는 주석 제거

--- workspace/ch02/ex02-19.js
+++ workspace/ch02/ex02-19.js
@@ -8,18 +8,6 @@ const getPingName = function () {
   return this.name;
   // 화살표 함수를 만들면 this를 생성하지 않음
 };
-/*
-const baro = new Object();
-baro.name = "바로핑";
-baro.age = 9;
-baro.getName = getPingName;
-
-const rara = {
-  name: "라라핑",
-  age: 8,
-  getName: getPingName,
-};
-*/
 
 // 객체를 생성해서 반환하는 함수(생성자)
 function Ping(name, age) {
@@ -29,12 +17,6 @@ function Ping(name, age) {
     return this.name;
   };
 }
-/*
-일반 함수
-// const baro = Ping("바로핑", 7);
-// const rara = Ping("라라핑", 8);
-// const copyPing = Ping("카피핑", 12);
-*/
 
 /**
  * new의 동작
@@ -49,9 +31,6 @@ const copyPing = new Ping("카피핑", 12);
 baro.age++;
 rara.age++;
 
-baro.height = "130";
-rara.height = "125";
-
-console.log("이 핑의 이름은:", baro.getName(), "| 나이는:", baro.age, "| 키는:", baro.height); // getName의 this는 baro
-console.log("이 핑의 이름은:", rara.getName(), "| 나이는:", rara.age, "| 키는:", rara.height); // getName의 this는 rara
+console.log("이 핑의 이름은:", baro.getName(), "| 나이는:", baro.age); // getName의 this는 baro
+console.log("이 핑의 이름은:", rara.getName(), "| 나이는:", rara.age); // getName의 this는 rara
 console.log("이 핑의 이름은:", copyPing.getName(), "| 나이는:", copyPing.age); // getName의 this는 copyPing
