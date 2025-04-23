33778b4b189217d8dbd51b870dc7a3c9b91fc626
koo-rogie <rn5184@naver.com>
2025-04-23 17:20:12 +0900

    📝Docs : 2025-04-23 배운것

---
+++ workspace/ch02/ex02-20.js
@@ -0,0 +1,39 @@
+/**
+ * 국어, 영어, 수학 점수를 받아서 총점과 평균을 계산할 수 있는 객체를 생성
+ * @param {number} kor 국어 점수
+ * @param {number} eng 영어 점수
+ * @param {number} math 수학 점수
+ */
+const Score = function (kor, eng, math) {
+  this.kor = kor;
+  this.eng = eng;
+  this.math = math;
+  this.sum = function () {
+    //TODO 클로저 단원 - 실행이 완료된 함수의 지역변수가 참조 가능한 상태고 유지되는 현상 발생
+    // return kor + eng + math;
+    return this.kor + this.eng + this.math;
+  };
+  this.avg = function () {
+    return this.sum() / 3;
+  };
+};
+
+// 바로핑은 점수가 각각 100, 90, 80이다
+const baro = new Score(100, 90, 80);
+// 라라핑은 점수가 각각 90, 80, 60이다
+const rara = new Score(90, 80, 60);
+
+
+// 두사람의 총점과 평균을 출력하세요
+console.log("총점은:", baro.sum(), "평균 점수는:", baro.avg()); // 270, 90
+console.log("총점은:", rara.sum(), "평균 점수는:", rara.avg()); // 230, 76.666..
+
+// 전원 수학 점수를 10점 올린 후 총점과 평균을 출력하세요
+baro.math += 10;
+rara.math += 10;
+// console.log(baro);
+// console.log(rara);
+console.log("-----------------------");
+
+console.log("총점은:", baro.sum(), "평균 점수는:", baro.avg()); // 280, 93.333..
+console.log("총점은:", rara.sum(), "평균 점수는:", rara.avg()); // 240, 80
--- workspace/ch03/ex03-01.js
+++ workspace/ch03/ex03-01.js
@@ -13,14 +13,50 @@
  * @param {number} kor 국어 점수
  * @param {number} eng 영어 점수
  */
-const Score = function(kor, eng){
 
+// Score 생성자 함수 정의
+const Score = function (kor, eng) {
+  this.kor = kor; // 국어 점수를 인스턴스 속성으로 저장
+  this.eng = eng; // 영어 점수를 인스턴스 속성으로 저장
+
+  // sum 메서드를 인스턴스마다 별도로 정의 (화살표 함수 사용)
+  this.sum = () => {
+    return kor + eng; // this.kor, this.eng 대신 지역 변수 kor, eng 사용
+  };
+
+  // 아래 avg는 주석 처리됨. 인스턴스마다 정의할 수도 있으나, prototype 방식이 더 효율적
+  // this.avg = () => {
+  //   return this.sum() / 2;
+  // };
 };
 
-const s1 = new Score(90, 80);
-const s2 = new Score(70, 50);
+// avg 메서드를 prototype에 정의 → 모든 인스턴스가 공유함
+Score.prototype.avg = function () {
+  return this.sum() / 2; // 생성자 내부에서 정의된 this.sum() 호출
+};
 
-console.log(s1.sum()); // 170
-console.log(s1.avg()); // 85
+const s1 = new Score(90, 80); // s1 인스턴스 생성 (sum = 170)
+const s2 = new Score(70, 50); // s2 인스턴스 생성 (sum = 120)
+
+console.log(s1.sum()); // 170, 화살표 함수 sum이 kor + eng 반환
+console.log(s1.avg()); // 85, sum() 결과를 2로 나눔
+
 console.log(s2.sum()); // 120
 console.log(s2.avg()); // 60
+
+// sum은 생성자 내부에 정의되어 인스턴스마다 다름
+console.log(s1.sum === s2.sum); // false
+
+// avg는 prototype에 정의되어 모든 인스턴스에서 공유됨
+console.log(s1.avg === s2.avg); // true
+
+// -------------------------
+
+console.log(s1.toString()); // [object Object]
+// toString()은 Object.prototype에 정의된 메서드이므로 프로토타입 체인을 따라 사용 가능
+
+console.log(s1.hasOwnProperty("sum")); // true
+// sum은 생성자 내부에서 직접 정의된 속성이므로 s1 인스턴스가 자체적으로 가지고 있음
+
+console.log(s1.hasOwnProperty("avg")); // false
+// avg는 prototype에 정의된 메서드이므로 s1의 own property는 아님 (프로토타입 체인을 통해 접근)
