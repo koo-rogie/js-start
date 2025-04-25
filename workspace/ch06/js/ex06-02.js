ac8da131f2d37cbeca5d77a223181714f7172631
koo-rogie <rn5184@naver.com>
2025-04-25 15:47:34 +0900

    📝Docs : 컴파일링 완료

--- workspace/ch06/js/ex06-02.js
+++ workspace/ch06/js/ex06-02.js
@@ -1,4 +1,18 @@
 "use strict";
 // 기본 타입
 (() => {
+    let str = "100";
+    let eag = 12;
+    let doun = true;
+    doun = false;
+    let nullVal = null;
+    let emtyVal = undefined;
+    let todo = { title: "TypeScript 공부", doun: false };
+    let todoList = ["JavaScript", "TypeScript"];
+    let todoList2 = ["React", "Naxt.js"];
+    // tuple: 길이가 고정되고 각 요소의 타임이 정의된 타입
+    let items = ["안녕", 1];
+    // any 타입 상관없이 대입이 됨
+    let userName = "이일구";
+    userName = 213;
 })();
--- workspace/js/workspace/ch06/ex06-01.js
+++ workspace/js/workspace/ch06/ex06-01.js
@@ -1,5 +1,6 @@
+"use strict";
 // 개발 환경 확인
-(function () {
+(() => {
     function hello(name) {
         return "Hello " + name;
     }
