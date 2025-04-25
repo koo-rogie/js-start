ff5ff64e0fad5679e5bb370a5c8d581cf9fa9025
koo-rogie <rn5184@naver.com>
2025-04-25 20:38:45 +0900

    💬 Comment: alert 스코프 테스트 및 경고 메시지 출력 확인 코드 작성

---
+++ workspace/ch04/ex04-07.js
@@ -0,0 +1,16 @@
+// var alert = "출입금지";
+// console.log(alert); // 경고창 출력
+
+// function fn() {
+//   var alert = "출입금지";
+//   console.log(alert); // 경고창 출력
+// }
+// fn();
+// alert("작업완료");
+
+(() => {
+  var alert = "출입금지";
+  console.log(alert); // 경고창 출력
+})();
+
+alert("작업완료");
