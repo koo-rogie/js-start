2f390a078f87e5ff871bc3c45cff698bf1fe653c
koo-rogie <rn5184@naver.com>
2025-04-30 19:38:36 +0900

    🚧 work : 일부 수정

--- workspace/ts-refactoring/ch01/ex01-07-02.ts
+++ workspace/ts-refactoring/ch01/ex01-07-02.ts
@@ -1,24 +1,23 @@
 (() => {
-/*
+  /*
 Symbol
 
 */
 
-// string
-var s1 = "hello";
-var s2 = "world";
-var s3 = "hello";
+  // string
+  var s1 = "hello";
+  var s2 = "world";
+  var s3 = "hello";
 
-console.log(s1 === s2); // false
-console.log(s2 === s3); // false
-console.log(s1 === s3); // true
+  console.log(s1 === s2); // false
+  console.log(s2 === s3); // false
+  console.log(s1 === s3); // true
 
-var s1 = Symbol("hello");
-var s2 = Symbol("world");
-var s3 = Symbol("hello");
+  var sym1 = Symbol("hello");
+  var sym2 = Symbol("world");
+  var sym3 = Symbol("hello");
 
-console.log(s1 === s2); // false
-console.log(s2 === s3); // false
-console.log(s1 === s3); // false
-
+  console.log(sym1 === sym2); // false
+  console.log(sym2 === sym3); // false
+  console.log(sym1 === sym3); // false
 })();
